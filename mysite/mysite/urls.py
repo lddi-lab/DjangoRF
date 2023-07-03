da3d258275d3e46ab6476c370bd8c03027ace1b4
Lucky Icban <lddicban@gmail.com>
2023-07-03 15:39:02 +0800

    added image field to API

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register('movies', MovieViewSet)
@@ -27,4 +29,4 @@ router.register('comedy', ComedyViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+]+static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
